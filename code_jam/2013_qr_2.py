d52012fc99650f526efd480eab3c940ed0d5f236
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-03-25 21:20:48 -0600

    problema 2 del 2013 no resuelto

--- code_jam/2013_qr_2.py
+++ code_jam/2013_qr_2.py
@@ -16,10 +16,58 @@ for t in range(test_case):
 
     y = "YES"
 
-    for i in range(n):
-        for j in range(m):
-            pass
+    finished = False
 
+    while not finished:
+        cutted = False 
+        ## CHECANDO FILA POR FILA
+        for i in range(n):
+            h = pattern[i][0]
+            cuttable = True
+            for j in range(m):
+                if h < pattern[i][j]:
+                    cuttable = False
+                    break
 
+            if cuttable:
+                for j in range(m):
+                    if h < lawn[i][j]:
+                        lawn[i][j] = h
+                        cutted = True
+
+        ## CHECANDO COLUMNA POR COLUMNA
+        for i in range(m):
+            h = pattern[0][i]
+            cuttable = True
+            for j in range(n):
+                if h < pattern[j][i]:
+                    cuttable = False
+                    break
+
+            if cuttable:
+                for j in range(n):
+                    if h < lawn[j][i]:
+                        lawn[j][i] = h
+                        cutted = True
+
+        different = False
+
+        for i in range(n):
+            has_different = False
+            for j in range(m):        
+                if lawn[i][j] != pattern[i][j]:
+                    different = True
+                    has_different = True
+                    break
+            if has_different:
+                break
+
+        if not different:
+            y = "YES"
+            finished = True
+
+        if not cutted:
+            y = "N0"
+            finished = True
 
     print(f"Case #{t + 1}: {y}")
